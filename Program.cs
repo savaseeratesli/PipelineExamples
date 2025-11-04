65f8c4d540c0461da70e975a11b5ff72f8bfb5ce
savaseeratesli <savaseeratesli@gmail.com>
2025-11-04 18:08:14 +0300

    ekran yenilendi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Selamlar tüm aktarımla geldi. Savaş Enes Erateşli");
+app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.");
 
 app.Run("http://0.0.0.0:9090");
