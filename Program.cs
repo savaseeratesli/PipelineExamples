d663e37ed37c05fe9189820e2b77a1a341bcb4b3
savaseeratesli <savaseeratesli@gmail.com>
2025-11-04 16:36:03 +0300

    ekrandaki yazı değişti

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from Savaş Enes Erateşli");
+app.MapGet("/", () => "Selamlar tüm aktarımla geldi. Savaş Enes Erateşli");
 
 app.Run("http://0.0.0.0:9090");
