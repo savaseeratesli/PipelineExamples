a674be3a3b9709036ae3a9c1bbd488f2e76e294b
savaseeratesli <savaseeratesli@gmail.com>
2025-11-05 11:57:14 +0300

    image tag v2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.Yeni sürüm");
+app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.Yeni sürüm v2");
 
 app.Run("http://0.0.0.0:9090");
