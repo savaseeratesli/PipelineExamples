15722455af3e3017247b5b5a24ae7034c2bc899f
savaseeratesli <savaseeratesli@gmail.com>
2025-11-05 15:55:49 +0300

    aktarım yapıldı

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "VS Code başlayan bu yolculuk k8s de son buldu!");
+app.MapGet("/", () => "VS Code başlayan bu yolculuk k8s de son buldu!Savaş Enes ERATEŞLİ");
 
 app.Run("http://0.0.0.0:9090");
