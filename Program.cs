a02b284250a496096729dda70e124d6dfdac2f34
savaseeratesli <savaseeratesli@gmail.com>
2025-11-05 13:10:54 +0300

    piplene ve kod düzenlendi.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.Yeni sürüm v2");
+app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.Tebrikler iyi dayandin.");
 
 app.Run("http://0.0.0.0:9090");
