dab3f62b5a4239945a510a96df847c5c73a70ebe
savaseeratesli <savaseeratesli@gmail.com>
2025-11-05 11:14:01 +0300

    imaj tarihi eklendi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.");
+app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.Yeni sürüm");
 
 app.Run("http://0.0.0.0:9090");
