2e880c9b5e31a24c8f9679bd86665800b4f91c74
savaseeratesli <savaseeratesli@gmail.com>
2025-11-05 14:30:46 +0300

    ekran değişti

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Selamlar CI/CD süreci tamamlandi.Tebrikler iyi dayandin.");
+app.MapGet("/", () => "VS Code başlayan bu yolculuk k8s de son buldu!");
 
 app.Run("http://0.0.0.0:9090");
