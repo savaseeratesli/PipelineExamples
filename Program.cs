4f7a9368518c5b1de2b466fb94447536a56ae7bc
savaseeratesli <savaseeratesli@gmail.com>
2025-11-06 23:31:29 +0300

    Ekran değişti

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Ziraat Team from @Savaş Enes ERATEŞLİ");
+app.MapGet("/", () => "Hello Team from @Savaş Enes ERATEŞLİ");
 
 app.Run("http://0.0.0.0:11130");
