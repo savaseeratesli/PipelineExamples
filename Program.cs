2df3ea80060fd3547a7d701a393a6c754375aa1f
savaseeratesli <savaseeratesli@gmail.com>
2025-11-06 23:13:27 +0300

    ekran güncellendi.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "hello world.Savaş Enes ERATEŞLİ.06/11/2025 16:35");
+app.MapGet("/", () => "Son güncelleme 23:13 6.11.25");
 
 app.Run("http://0.0.0.0:9090");
